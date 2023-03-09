c17d5252de0191b3fa8af89ca688ba727d76b138
Antoinebac435 <90193391+Antoinebac435@users.noreply.github.com>
2023-03-09 15:07:20 +0100

    4 files modified, 6 added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ProductService {
-  constructor(private http: HttpClient) {}
 
-  private productsUrl = 'http://80.11.122.222:8000/polyfx/cgi/getart.cgi?frs=';
-  private fournisseurId: string = '401000208';
-  private products: Product[] = [];
+  constructor() { }
 }
-
-getProducts(): Observable<Product[]> {}
